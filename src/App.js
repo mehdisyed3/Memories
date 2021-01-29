a9160f39cf6ce1f8e806099dc8341d79b1734548
Syed Mehdi <mehdisyed3@gmail.com>
2021-01-29 13:27:23 -0500

    created components, more work to be don on them

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import './App.css';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import Posts from './Posts/Posts';
 
 function App() {
   return (
     <div className="app">
-      hello Oni
+      <Container maxWidth='lg'>
+        <AppBar position='static' color='inherit'>
+          <Typography variant='h2' align='center' >
+            Memories
+          </Typography>
+          <img src='https://raw.githubusercontent.com/adrianhajdin/project_mern_memories/master/client/src/images/memories.png?token=AF56X74XONEUGZ4FD2FUIA27UURPI' alt='' height='60' />
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                
+              </Grid>
+            </Grid>
+          </Container>
+
+        </Grow>
+      </Container>
     </div>
   );
 }
---
+++ src/Form/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Form.css'
+
+function Form() {
+  return (
+    <div className="form">
+      FORM
+    </div>
+  )
+}
+
+export default Form
